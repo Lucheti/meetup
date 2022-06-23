fffa2679d4e7f7ca0e1f7495c123ab3d0f08a4af
lucheti <lucas.garcia@salesforce.com>
2022-06-23 18:12:03 -0300

    page errors

--- app/events/pages/events/join/[eventId].tsx
+++ app/events/pages/events/join/[eventId].tsx
@@ -1,11 +1,81 @@
 import { BlitzPage, Routes, useMutation, useParam, useRouter } from "blitz"
 import Layout from "../../../../core/layouts/Layout"
-import { Button, Center, Container, Group, LoadingOverlay, Paper, Text, Title } from "@mantine/core"
-import { useCurrentUser } from "../../../../core/hooks/useCurrentUser"
+import {
+  Button,
+  Center,
+  Container,
+  createStyles,
+  Group,
+  LoadingOverlay,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core"
 import joinEventMutation from "../../../mutations/joinEvent"
 import React, { useEffect } from "react"
 import { showNotification } from "@mantine/notifications"
-import { useJoinEventStyles } from "./styles"
+
+const useJoinEventStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  inner: {
+    position: "relative",
+  },
+
+  image: {
+    position: "absolute",
+    top: -50,
+    right: 0,
+    left: 0,
+    zIndex: 0,
+    opacity: 0.3,
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  content: {
+    paddingTop: 220,
+    position: "relative",
+    zIndex: 1,
+
+    [theme.fn.smallerThan("sm")]: {
+      paddingTop: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 32,
+    },
+  },
+  label: {
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 450,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 120,
+    },
+  },
+
+  description: {
+    maxWidth: 540,
+    margin: "auto",
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}))
 
 export const JoinEvent: BlitzPage = () => {
   const eventId = useParam("eventId", "string")
