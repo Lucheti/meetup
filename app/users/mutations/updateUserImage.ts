88ea938b662f56a2c21f50d5efd8b44f04a77b12
lucheti <lucas.garcia@salesforce.com>
2022-07-03 12:44:39 -0300

    added tickets, pricing and studio in prod

--- app/users/mutations/updateUserImage.ts
+++ app/users/mutations/updateUserImage.ts
@@ -3,7 +3,7 @@ import db, { Images } from "db"
 
 export default resolver.pipe(
   resolver.authorize(),
-  async ({ images }: { images: Omit<Images, "id" | "userId"> }, ctx) => {
+  async ({ images }: { images: Omit<Images, "id" | "userId" | "providerId" | "eventId"> }, ctx) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
 
     await db.images.upsert({
